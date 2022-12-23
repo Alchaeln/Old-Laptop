f4e72707d0594fdcb3b0c75e3d3acc186e2b7043
Alchaeln <116860177+Alchaeln@users.noreply.github.com>
2022-12-22 20:54:31 -0700

    Add files via upload

---
+++ TicTacToe Simulation/TicTacToe Simulation/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe_Simulation
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+            int[,] intArray = new int[3, 3];
+            //press button to simulate game
+            //get new game
+            //shoot out array 3 by 3
+            //fill the array with a one or a zero
+            Random random = new Random();
+
+
+
+            for (int row = 0; row < 3; row++)
+            {
+                label1.Text += "\n| ";
+                //int temp = random.Next(0, 2);
+                for (int col = 0; col < 3; col++)
+                {
+                    int i = random.Next(0, 2);
+
+                    if (i == 1)//1 = X
+                    {
+                        intArray[row, col] = 1;
+                        label1.Text += (" X | ");
+                    }
+                    else if (i == 0)//0 = O
+                    {
+                        intArray[row, col] = 0;
+                        label1.Text += (" O | ");
+                    }
+
+                }
+
+                //Once the array is checked if three are in a row
+                //if none are  in a  row then it is a draw
+
+            }
+
+
+            if (intArray[0, 0] == 0 && intArray[0, 1] == 0 && intArray[0, 2] == 0 || intArray[1, 0] == 0 && intArray[1, 1] == 0 && intArray[1, 2] == 0
+                || intArray[2, 0] == 0 && intArray[2, 1] == 0 && intArray[2, 2] == 0|| intArray[0, 2] == 0 && intArray[1, 2] == 0 && intArray[2, 2] == 0
+                || intArray[0, 0] == 0 && intArray[1, 0] == 0 && intArray[2, 0] == 0|| intArray[0, 1] == 0 && intArray[1, 1] == 0 && intArray[2, 1] == 0 
+                || intArray[0, 2] == 0 && intArray[1, 2] == 0 && intArray[2, 2] == 0|| intArray[0, 0] == 0 && intArray[1, 1] == 0 && intArray[2, 2] == 0
+                || intArray[0, 2] == 0 && intArray[1, 1] == 0 && intArray[2, 0] == 0)
+            {
+                label2.Text = ("O is the Winner!");
+            }
+            else if (intArray[0, 0] == 1 && intArray[0, 1] == 1 && intArray[0, 2] == 1|| intArray[1, 0] == 1 && intArray[1, 1] == 1 && intArray[1, 2] == 1
+                || intArray[2, 0] == 1 && intArray[2, 1] == 1 && intArray[2, 2] == 1|| intArray[0, 2] == 1 && intArray[1, 2] == 1 && intArray[2, 2] == 1
+                || intArray[0, 0] == 1 && intArray[1, 0] == 1 && intArray[2, 0] == 1|| intArray[0, 1] == 1 && intArray[1, 1] == 1 && intArray[2, 1] == 1
+                || intArray[0, 2] == 1 && intArray[1, 2] == 1 && intArray[2, 2] == 1|| intArray[0, 0] == 1 && intArray[1, 1] == 1 && intArray[2, 2] == 1
+                || intArray[0, 2] == 1 && intArray[1, 1] == 0 && intArray[2, 0] == 1)
+            {
+                label2.Text = ("X is the Winner!");
+            }
+            else
+            {
+                label2.Text = ("Game was a Tie!");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace WindowsFormsApp
+{
+    partial class ThankYou
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.lblMessage2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(75, 114);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 3;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(78, 151);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 4;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(78, 180);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(168, 114);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFirstName_TextChanged);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(168, 151);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(307, 301);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(178, 23);
+            this.btnMessage.TabIndex = 2;
+            this.btnMessage.Text = "Display Message";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // lblMessage2
+            // 
+            this.lblMessage2.AutoSize = true;
+            this.lblMessage2.Location = new System.Drawing.Point(78, 52);
+            this.lblMessage2.Name = "lblMessage2";
+            this.lblMessage2.Size = new System.Drawing.Size(318, 13);
+            this.lblMessage2.TabIndex = 6;
+            this.lblMessage2.Text = "Enter your first and last name, then hit the display message button.";
+            // 
+            // ThankYou
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 413);
+            this.Controls.Add(this.lblMessage2);
+            this.Controls.Add(this.btnMessage);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "ThankYou";
+            this.Text = "Thank You";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnMessage;
+        private System.Windows.Forms.Label lblMessage2;
+    }
+}
+
